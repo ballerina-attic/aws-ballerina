62578ee7bf127b8b6fb570b57a06443c9a4c1ce6
Manuri <amaya.uom@gmail.com>
2019-03-05 15:31:41 +0530

    Remove cluster creation step

--- data-integration/provision-infrastructure-default.sh
+++ data-integration/provision-infrastructure-default.sh
@@ -24,6 +24,6 @@ grand_parent_path=$(dirname ${parent_path})
 output_dir=$2
 echo ${output_dir}
 
-create_default_cluster_and_write_infra_properties ${output_dir}
+#create_default_cluster_and_write_infra_properties ${output_dir}
 
 create_default_database_and_write_infra_properties ${output_dir} "db.t2.micro"
